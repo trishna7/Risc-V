406eea1ae11e005302bedc72730663daa95a1cef
trishna7 <trishna722.ts33@gmail.com>
2025-02-25 14:51:43 +0545

    instruction to synchronous

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -4,11 +4,12 @@ module Instruction_Memory (
     output [31:0] instruction_out
 );
     // Making Memory
-    reg [31:0] memory [0:25];
+    reg [31:0] memory [0:255];
 
     initial begin
-        $readmemh("instruction.mem", memory); // load instruction file instruction.mem
+        $readmemh("instruction.hex", memory); // load instruction file instruction.mem
     end
 
-    assign instruction_out = memory[read_address[31:2]]; 
+    always @(*) begin
+     instruction_out = memory[read_address[31:2]]; 
 endmodule
